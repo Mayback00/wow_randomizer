a5997bdf7ed5d644726a9962a442d6ae3230e7e2
Mayback00 <jei200289@gmail.com>
2023-11-22 20:12:02 -0300

    "New content and code modifications"

--- wow_ramdomizer_0.1.py
+++ wow_ramdomizer_0.1.py
@@ -14,84 +14,63 @@ class WoWCharacterGenerator:
         self.label_fondo = tk.Label(root, image=self.fondo)
         self.label_fondo.place(relwidth=1, relheight=1)
 
-        # Razas, géneros y clases
-        self.razas = ['Humano', 'Elfo_de_la_noche', 'Enano', 'Gnomo', 'Orco', 'Trol', 'No-muerto', 'Tauren', 'Elfo_de_sangre', 'Elfo_nato_nocturno', 'Draenei', 'Draenei_temple_luz', 'Goblin', 'Kultiriano', 'Pandaren', 'Mecagnomo', 'Orco_maghar', 'Tauren_alta_montana', 'Trol_zandalary', 'Drakthyr', 'Vulpira', 'Enano_roca_negra', 'Huargen', 'Elfo_del_vacio']
-        self.generos = ['Masculino', 'Femenino']
-        self.clases = ['Guerrero', 'Paladin', 'Cazador', 'Picaro', 'Sacerdote', 'Dk', 'Chaman', 'Mago', 'Brujo', 'Monje', 'Druida', 'Evocador', 'Dh']
+        # Crear un marco para el grid de imágenes
+        self.marco_imagenes = ttk.Frame(root)
+        self.marco_imagenes.grid(row=0, column=0, pady=(root.winfo_reqheight() * 0.1, 0), padx=(0, 0), sticky='n')
 
-        # Cargar imágenes de razas, géneros y clases
+        # Razas, géneros y clases (incluyendo las opciones adicionales)
+        self.razas = ['Humano', 'Elfo_de_la_noche', 'Enano', 'Gnomo', 'Orco', 'Trol', 'No-muerto', 'Tauren', 'Elfo_de_sangre', 'Elfo_nato_nocturno', 'Draenei', 'Draenei_temple_luz', 'Goblin', 'Kultiriano', 'Pandaren', 'Mecagnomo', 'Orco_maghar', 'Tauren_alta_montana', 'Trol_zandalary', 'Drakthyr', 'Vulpira', 'Enano_roca_negra', 'Huargen', 'Elfo_del_vacio', 'Facciones']
+        self.generos = ['Masculino', 'Femenino', 'mf']
+        self.clases = ['Guerrero', 'Paladin', 'Cazador', 'Picaro', 'Sacerdote', 'Dk', 'Chaman', 'Mago', 'Brujo', 'Monje', 'Druida', 'Evocador', 'Dh', 'Tdah']
+
+        # Listas de opciones adicionales
+        opciones_adicionales_razas = ['Facciones']
+        opciones_adicionales_generos = ['Mf']
+        opciones_adicionales_clases = ['Tdah']
+
+        # Cargar imágenes de razas, géneros y clases (incluyendo las opciones adicionales)
         self.imagenes_razas = [ImageTk.PhotoImage(Image.open(f'imagenes/imagen_raza_{raza.lower()}.png').resize((134, 131), resample=Image.LANCZOS)) for raza in self.razas]
         self.imagenes_generos = [ImageTk.PhotoImage(Image.open(f'imagenes/imagen_genero_{genero.lower()}.png').resize((134, 131), resample=Image.LANCZOS)) for genero in self.generos]
         self.imagenes_clases = [ImageTk.PhotoImage(Image.open(f'imagenes/imagen_clase_{clase.lower()}.png').resize((134, 131), resample=Image.LANCZOS)) for clase in self.clases]
 
-        # Crear etiquetas e imágenes
-        self.imagen_raza = ttk.Label(root, image=None)
-        self.imagen_raza.grid(row=0, column=0, pady=(self.root.winfo_reqheight() * 0.1, 0), padx=(0, 0), sticky='n')
+        # Crear etiquetas e imágenes dentro del marco
+        self.imagen_raza = ttk.Label(self.marco_imagenes, image=self.imagenes_razas[-1])  # Última imagen es 'Facciones'
+        self.imagen_raza.grid(row=0, column=0, pady=0, padx=(0, 0), sticky='n')
 
-        self.imagen_genero = ttk.Label(root, image=None)
+        self.imagen_genero = ttk.Label(self.marco_imagenes, image=self.imagenes_generos[-1])  # Última imagen es 'Mf'
         self.imagen_genero.grid(row=1, column=0, pady=0, padx=(0, 0), sticky='n')
 
-        self.imagen_clase = ttk.Label(root, image=None)
-        self.imagen_clase.grid(row=2, column=0, pady=(0, 0), padx=(0, 0), sticky='n')
+        self.imagen_clase = ttk.Label(self.marco_imagenes, image=self.imagenes_clases[-1])  # Última imagen es 'Tdah'
+        self.imagen_clase.grid(row=2, column=0, pady=0, padx=(0, 0), sticky='n')
 
         # Botón de generación aleatoria
         self.boton_generar = ttk.Button(root, text="Generar Personaje", command=self.generar_personaje, style='TButton')
 
         # Obtener el 20% del borde superior
-        base_20_percent = int(self.root.winfo_reqheight() * 0.5)
+        base_20_percent = int(root.winfo_reqheight() * 0.5)
 
         # Configurar el botón en la base centrado
         self.boton_generar.place(relx=0.5, rely=1, anchor='s', y=-base_20_percent)
 
         # Establecer tamaño inicial
-        self.root.geometry("405x720")
+        root.geometry("405x720")
         # Vincular la función de ajuste al cambio de tamaño
-        self.root.bind("<Configure>", self.on_configure)
-        self.resize_flag = False
 
-    def on_configure(self, event):
-        # Check the flag to determine whether to adjust the size
-        if self.resize_flag:
-            # Ajustar la imagen de fondo al tamaño de la ventana
-            ancho, altura = 405, 720
-            nueva_fondo = Image.open("fondo.jpg").resize((ancho, altura), resample=Image.LANCZOS)
-            nueva_fondo = ImageTk.PhotoImage(nueva_fondo)
-            self.label_fondo.configure(image=nueva_fondo)
-            self.label_fondo.image = nueva_fondo
-
-            # Configurar la geometría de la ventana y deshabilitar la capacidad de cambiar tamaño
-            self.root.geometry(f"{ancho}x{altura}")
-            self.root.resizable(width=False, height=False)
-
-            # Configurar el tamaño de la cuadrícula para las imágenes
-            ancho_imagen = 134
-            altura_imagen = 134
-            margen_entre_imagenes = (altura - 3 * altura_imagen) // 2  # Margen entre las secciones
-
-            # Configurar la cuadrícula para centrar verticalmente las imágenes
-            self.root.grid_rowconfigure(0, minsize=altura_imagen, weight=1)
-            self.root.grid_rowconfigure(1, minsize=margen_entre_imagenes, weight=0)
-            self.root.grid_rowconfigure(2, minsize=altura_imagen, weight=1)
-
-            # Centrar las imágenes horizontalmente en la cuadrícula
-            self.imagen_raza.grid_configure(pady=(0, 15))
-            self.imagen_genero.grid_configure(pady=(0, 0))
-            self.imagen_clase.grid_configure(pady=(15, 0))
-
-            # Ajustar solo el botón al 20% de la base
-            base_20_percent = int(self.root.winfo_reqheight() * 0.2)
-            self.boton_generar.place(relx=0.5, rely=1, anchor='s', y=-base_20_percent)
-
-            # Set the flag to False to prevent further adjustments
-            self.resize_flag = False
+        # Centrar el marco en la ventana
+        self.marco_imagenes.place(relx=0.5, rely=0.5, anchor='center')
+        # Vincular la función de ajuste al cambio de tamaño
 
     def generar_personaje(self):
         # Seleccionar al azar una raza, género y clase
-        raza_aleatoria = random.choice(self.razas)
-        genero_aleatorio = random.choice(self.generos)
+        razas_sin_opciones_adicionales = [raza for raza in self.razas if raza not in ['Facciones']]
+        generos_sin_opciones_adicionales = [genero for genero in self.generos if genero not in ['mf']]
+        clases_sin_opciones_adicionales = [clase for clase in self.clases if clase not in ['Tdah']]
+
+        raza_aleatoria = random.choice(razas_sin_opciones_adicionales)
+        genero_aleatorio = random.choice(generos_sin_opciones_adicionales)
 
         # Restricciones de clase según la raza
-        clases_permitidas = self.clases.copy()
+        clases_permitidas = clases_sin_opciones_adicionales.copy()
 
         if raza_aleatoria in ['Humano', 'Draenei_temple_luz']:
             clases_permitidas.remove('Chaman')
@@ -169,15 +148,6 @@ class WoWCharacterGenerator:
         self.imagen_genero.configure(image=self.imagenes_generos[self.generos.index(genero_aleatorio)])
         self.imagen_clase.configure(image=self.imagenes_clases[self.clases.index(clase_aleatoria)])
 
-        self.root.grid_columnconfigure(0, weight=1)
-        self.root.grid_rowconfigure(0, weight=0)
-        self.root.grid_rowconfigure(1, weight=0)
-        self.root.grid_rowconfigure(2, weight=2)
-
-        # Ajustar el espacio vertical
-        self.imagen_raza.grid_configure(column=0, sticky='n')
-        self.imagen_genero.grid_configure(column=0, sticky='n')
-        self.imagen_clase.grid_configure(column=0, sticky='n')
 
 if __name__ == "__main__":
     root = tk.Tk()
